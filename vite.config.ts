2753ee891f0d7cd5b3fc11bb8e0fcec8132549d3
Marius Zell-Flagstad(server) <marius_flagstad@hotmail.com>
2026-02-08 11:25:14 +0000

    🔧 Configure Vite alias for ~ path

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import { remix } from '@remix-run/dev'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [remix()],
+  resolve: {
+    alias: {
+      '~': fileURLToPath(new URL('./app', import.meta.url))
+    }
+  }
 })
